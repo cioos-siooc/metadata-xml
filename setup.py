6adb3b00ef1703f521d7a3277a570856c903be99
Nate <nate.rosenstock@hakai.org>
2019-10-31 10:52:22 -0700

    switch to setup.py for installation

---
+++ setup.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='metadata_xml',
+      version='0.1',
+      description='Python module for converting ACDD style metadata into the CIOOS ISO profile',
+      url='https://github.com/cioos-siooc/metadata-xml',
+      packages=['metadata_xml', ],
+      include_package_data=True,
+
+      install_requires=['Jinja2 == 2.10.3',
+                        'lxml == 4.4.1',
+                        'PyYAML == 5.1.2'
+                        ]
+
+      )
