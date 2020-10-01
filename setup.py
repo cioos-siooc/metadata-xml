ad1f8211afcbd3f0a082a75f9db27a8c042496a8
Nate <nate.rosenstock@hakai.org>
2020-10-01 11:05:30 -0700

    remove extra comma

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ setup(name='metadata_xml',
       description='Python module for converting ACDD style metadata into the '
       + 'CIOOS ISO profile',
       url='https://github.com/cioos-siooc/metadata-xml',
-      packages=['metadata_xml', ],
+      packages=['metadata_xml'],
       include_package_data=True,
 
       install_requires=['Jinja2 == 2.10.3',
