f89152e2a3ff1621e13b9a06367b9e73dc87bdd3
Jessy Barrette <30420025+JessyBarrette@users.noreply.github.com>
2025-04-04 10:33:00 -0400

    improve tests

--- tests/test_conversion.py
+++ tests/test_conversion.py
@@ -8,7 +8,6 @@ from lxml import etree
 SAMPLE_RECORDS_DIR = Path(__file__).parent / ".." / "sample_records"
 SAMPLE_RECORDS = SAMPLE_RECORDS_DIR.glob("*.yml")
 
-XSD_DIR = Path(__file__).parent / ".." / "xsd"
 MBD_SCHEMA = Path(__file__).parent / "../xsd/schemas.isotc211.org/19115/-3/mdb/2.0/mdb.xsd"
     
 
@@ -19,14 +18,16 @@ def test_sample_records(record_file, tmp_path):
         record = yaml.safe_load(stream)
 
     xml_string = metadata_to_xml(record)
-    
-    # Save the XML string to a temporary file
-    temp_file = tmp_path / record_file.name.replace(".yml", ".xml")
-    temp_file.write_text(xml_string, encoding="utf-8")
-    
-    # Check that the XML string is not empty
     assert xml_string.strip(), "XML string is empty"
 
+    # Write result just to review
+    xml_file =  record_file.parent / f"{record_file.stem}.xml"
+    xml_file.write_text(xml_string, encoding="utf-8")
+
+    xml_doc = etree.XML(xml_string.encode("utf-8"))
+    assert xml_doc is not None, "XML document is None"
+    assert isinstance(xml_doc, etree._Element), "XML document is not an Element"
+
+
     schema = etree.XMLSchema(file=MBD_SCHEMA)
-    xml_doc = etree.parse(temp_file)
     schema.assertValid(xml_doc)
